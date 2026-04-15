daf1d01ee1a1dcb685db88ee097318615cf7df05
Offir Shvartz <offir.shvartz@cyberark.com>
2026-04-15 15:33:19 +0300

    chore: bump to v0.1.0

--- Casks/reviewflow.rb
+++ Casks/reviewflow.rb
@@ -1,6 +1,6 @@
 cask "reviewflow" do
-  version "0.0.9"
-  sha256 "11a479f49dc1df849be5c0f1701f4cf49e4c4e0205c62ece02fc8861418014a9"
+  version "0.1.0"
+  sha256 "64239473cc68a3523ea131f34db5c6a258d755df8a7e7cd9a397c0b44d172ada"
 
   url "https://github.com/oshvartz/homebrew-reviewflow/releases/download/v#{version}/reviewFlow_#{version}_aarch64.dmg"
 
